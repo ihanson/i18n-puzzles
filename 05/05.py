ec2891fc1c6a44137d8becca8e94b2ec84c601f2
Ira Hanson <hanson.ira@gmail.com>
2025-03-11 10:51:50 -0500

    Puzzle 5

--- 05/05.py
+++ 05/05.py
@@ -3,7 +3,14 @@ from typing import Iterator
 use_test = False
 
 def run(lines: Iterator[str]):
-	...
+	park = [list(line) for line in lines]
+	(x, y) = (0, 0)
+	count = 0
+	while y < len(park):
+		if park[y][x] == "💩":
+			count += 1
+		(x, y) = ((x + 2) % len(park[y]), y + 1)
+	print(count)
 
 #region Common code
 if __name__ == "__main__":
